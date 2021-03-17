5c91bc3fe16ac8a89abe00dfe85ff01831151c7e
Bikram Chandf <chandbikram001@gmail.com>
2021-03-17 19:11:17 +0545

    initial commit

---
+++ app/src/main/java/com/example/android_todo_first/TodoActivity.java
@@ -0,0 +1,50 @@
+package com.example.android_todo_first;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class TodoActivity extends AppCompatActivity {
+    private static final String TODO_INDEX = "todoIndex";
+    private String[] mTodos;
+    private int mTodoIndex = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo);
+        // initialize member TextView so we can manipulate it later
+        final TextView TodoTextView;
+        TodoTextView = (TextView) findViewById(R.id.textViewTodo);
+
+        // read the todo array from res/values/strings.xml
+        Resources res = getResources();
+        mTodos = res.getStringArray(R.array.todos);
+
+        // display the first task from mTodo array in the TodoTextView
+        TodoTextView.setText(mTodos[mTodoIndex]);
+
+        Button buttonNext;
+        buttonNext = (Button) findViewById(R.id.buttonNext);
+
+        // OnClick listener for the  Next button
+        buttonNext.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                mTodoIndex = (mTodoIndex + 1) % mTodos.length;
+                TodoTextView.setText(mTodos[mTodoIndex]);
+            }
+        });
+        if (savedInstanceState != null){
+            mTodoIndex = savedInstanceState.getInt(TODO_INDEX, 0);
+        }
+    }
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt(TODO_INDEX, mTodoIndex);
+    }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+### First Todo App
+#### Demonstration
+![](./video.gif)
